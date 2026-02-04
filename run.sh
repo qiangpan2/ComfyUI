7cac31f6b5e07919e4434321f253137d5331ef0f
qiang <qiangpan@amd.com>
2026-02-04 17:56:52 +0800

    update

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ export PYTORCH_MIOPEN_SUGGEST_NHWC=1
 export AMD_LOG_LEVEL=3
 
 #ShaderName : void ck_tile::kentry<ck_tile::gfx11_t, 2, ck_tile::GroupedConvolutionForwardKernel
-uv run  main.py --listen --port 2828 --force-channels-last --bf16-vae --bf16-unet 2>&1 | tee output.log
+uv run python  main.py --listen --port 2828 --force-channels-last --bf16-vae --bf16-unet  --use-flash-attention 2>&1 | tee output.log
